6b6fa57b8e190b475b0937e4f8efbeabc6066a99
Joe Querin <joecue@gmail.com>
2016-11-30 09:46:01 -0500

    Accessibility Fixes
    Color Contrast Ratio Fixed
    Hover color of footer links now white
    Title attributes on all links for footer
    Title attributes on breadcrumb links

--- footer.php
+++ footer.php
@@ -9,36 +9,36 @@
 ?>
 	</div><!-- #content -->
 
-	<footer id="colophon" class="small-12 medium-12 large-12 columns site-footer" role="contentinfo">
+		<footer id="colophon" class="small-12 medium-12 large-12 columns site-footer hide-for-print" role="contentinfo">
 		  <div class="row text-center medium-text-left">
-    <div class="large-3 medium-3 columns"><a href="/"><img class="footer-logo" src="<?php bloginfo('stylesheet_directory'); ?>/images/icons/lccclogo_white.svg" alt="" width="220" height="42.5"/></a>
+    <div class="large-3 medium-3 columns"> <a href="/" title="Lorain County Community College Home"><img class="footer-logo" src="<?php bloginfo('stylesheet_directory'); ?>/images/icons/lccclogo_white.svg" alt="" width="220" height="42.5"/></a>
       <h2>Connect with LCCC</h2>
       <ul class="menu footer-sm-links">
-        <li><a href="http://facebook.com/lorainccc" target="_blank"><img src="<?php bloginfo('stylesheet_directory'); ?>/images/icons/facebook_white.svg" height="30" width="30" alt="" /></a></li>
-        <li><a href="http://twitter.com/lorainccc" target="_blank"><img src="<?php bloginfo('stylesheet_directory'); ?>/images/icons/twitter_white.svg" height="30" width="30" alt="" /></a></li>
-        <li><a href="http://www.linkedin.com/company/lorain-county-community-college" target="_blank"><img src="<?php bloginfo('stylesheet_directory'); ?>/images/icons/linkedin_white.svg" height="30" width="30" alt="" /></a></li>
+        <li><a href="http://facebook.com/lorainccc" title="Follow LCCC on Facebook" target="_blank"><img src="<?php bloginfo('stylesheet_directory'); ?>/images/icons/facebook_white.svg" height="30" width="30" alt="" /></a></li>
+        <li><a href="http://twitter.com/lorainccc" title="Follow LCCC on Twitter" target="_blank"><img src="<?php bloginfo('stylesheet_directory'); ?>/images/icons/twitter_white.svg" height="30" width="30" alt="" /></a></li>
+        <li><a href="http://www.linkedin.com/company/lorain-county-community-college" title="Follow LCCC on LinkedIn" target="_blank"><img src="<?php bloginfo('stylesheet_directory'); ?>/images/icons/linkedin_white.svg" height="30" width="30" alt="" /></a></li>
         <!--<li><a href="#" target="_blank"><img src="<?php bloginfo('stylesheet_directory'); ?>/images/icons/pinterest_white.svg" height="30" width="30" alt="" /></a></li>-->
-        <li><a href="http://www.instagram.com/lorainccc" target="_blank"><img src="<?php bloginfo('stylesheet_directory'); ?>/images/icons/instagram_white.svg" height="30" width="30" alt="" /></a></li>
+        <li><a href="http://www.instagram.com/lorainccc" title="Follow LCCC on Instagram" target="_blank"><img src="<?php bloginfo('stylesheet_directory'); ?>/images/icons/instagram_white.svg" height="30" width="30" alt="" /></a></li>
       </ul>
-      <a href="/campus-technology/lccc-mobile/" target="_blank" class="clearfix mobile-app-link"><img src="<?php bloginfo('stylesheet_directory'); ?>/images/icons/smartphone_yellow.svg" heigth="33" width="20" alt=""/>
+      <a href="/campus-technology/lccc-mobile/" title="Download LCCC's Mobile App" target="_blank" class="clearfix mobile-app-link"><img src="<?php bloginfo('stylesheet_directory'); ?>/images/icons/smartphone_yellow.svg" heigth="33" width="20" alt=""/>
       <h2>LCCC'S<br />
         Mobile App</h2>
       </a>
      <p class="website-feedback">
-      <a href="/website-feedback?siteurl=<?php echo 'https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']; ?>" target="_blank">Feedback about this page</a>
+      <a href="/website-feedback?siteurl=<?php echo 'https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']; ?>" title="Provide Feedback about this Page" target="_blank">Feedback about this page</a>
      </p>
      </div>
-   <div class="large-3 medium-3 columns">
+    <div class="large-3 medium-3 columns">
       <h2>Contact LCCC</h2>
       <p>1005 N Abbe Rd<br />
         Elyria, OH 44035<br />
         1-800-995-LCCC (5222)<br />
 							 or (440) 365-5222<br />
-        <a href="mailto:info@lorainccc.edu">info@lorainccc.edu</a> </p>
-      <ul class="underline">
-        <li><a href="/about/map-and-directions-to-lccc/">Map and Directions</a></li>
-        <li><a href="/about/contact-lorain-county-community-college/">Contact LCCC</a></li>
-        <li><a href="#">Visit LCCC</a></li>
+        <a href="mailto:info@lorainccc.edu" title="Email LCCC for more information">info@lorainccc.edu</a> </p>
+      <ul class="underline"> 
+        <li><a href="/about/map-and-directions-to-lccc/" title="Map and Directions to LCCC">Map and Directions</a></li>
+        <li><a href="/about/contact-lorain-county-community-college/" title="Contact LCCC">Contact LCCC</a></li>
+        <li><a href="/getting-started/campus-visitation-days/" title="Visit LCCC">Visit LCCC</a></li>
       </ul>
     </div>
     <div class="large-3 medium-3 columns">
